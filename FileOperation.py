38090108e8d284e426220431827f9af971753741
Bingo-Li <2260334227@qq.com>
2019-03-30 17:41:40 +0800

    change

---
+++ FileOperation.py
@@ -0,0 +1,13 @@
+import os,shutil 
+
+print('输入格式：E:\myprojectnew\jupyter\整理文件夹\示例')
+path = input('请键入需要整理的文件夹地址：')
+new_path = input('请键入要复制到的文件夹地址：')
+
+for root, dirs, files in os.walk(path):  
+    for i in range(len(files)):
+        if (files[i][-3:] == 'jpg') or (files[i][-3:] == 'png') or (files[i][-3:] == 'JPG'):
+            file_path = root+'/'+files[i]  
+            new_file_path = new_path+ '/'+ files[i]  
+            shutil.copy(file_path,new_file_path) 
+        
